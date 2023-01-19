7e36a40068e6b4523bd287510da1145fcec9c374
Kapitalist-RF <den.ry97@gmail.com>
2023-01-19 19:46:49 +0300

    Comparator Finish

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Person> arrayList = new ArrayList<>();
+        Person person01 = new Person("Tanya", "Ivanova", 20);
+        Person person02 = new Person("Petya", "Ivanova", 25);
+        Person person03 = new Person("Sonya", "Vasilyev-Slon", 30);
+        Person person04 = new Person("Sonya", "Vasilyeva", 22);
+        Person person05 = new Person("Sonya", "Vasily Gol", 19);
+        Person person06 = new Person("Sonya", "Vasilisovna", 22);
+        arrayList.add(person01);
+        arrayList.add(person02);
+        arrayList.add(person03);
+        arrayList.add(person04);
+        arrayList.add(person05);
+        arrayList.add(person06);
+        Collections.sort(arrayList, new PersonComparator(2));
+        System.out.println(arrayList);
+
+    }
+
+}
---
+++ src/PersonComparator.java
@@ -0,0 +1,37 @@
+import java.util.Comparator;
+
+public class PersonComparator implements Comparator<Person> {
+
+    private int countWords;
+
+    public PersonComparator(int countWords) {
+        this.countWords = countWords;
+    }
+
+    @Override
+    public int compare(Person o1, Person o2) {
+        int count = o1.getSurname().split("\\P{IsAlphabetic}+").length +
+                o2.getSurname().split("\\P{IsAlphabetic}+").length;
+        if (count > countWords) {
+            if (o1.getAge() < o2.getAge()) {
+                return -1;
+            } else if (o1.getAge() > o2.getAge()) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else if (o1.getSurname().length() < o2.getSurname().length()) {
+            return -1;
+        } else if (o1.getSurname().length() > o2.getSurname().length()) {
+            return 1;
+        } else {
+            if (o1.getAge() < o2.getAge()) {
+                return -1;
+            } else if (o1.getAge() > o2.getAge()) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+}
